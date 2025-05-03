225b0f6f50088a6281b32e7b3a280395a5bf200f
Mabarik667f <jc.bob1412@gmail.com>
2025-05-03 17:45:05 +0300

    feat: base options bluetoothctl, get_devices

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use std::process::{Command, Output};
+use core::time;
+use std::{
+    process::{Command, Output},
+    thread,
+};
 
 #[derive(Debug)]
 struct ServiceAction<'a> {
@@ -21,7 +25,24 @@ impl<'a> BluetoothCtlRunner for ServiceAction<'a> {
 }
 
 pub fn toggle_power(power_off: bool) {
-    let flag = if power_off { "on" } else { "off" };
+    let flag: &str;
+    if power_off {
+        let bl_locked = ServiceAction {
+            arg: &format!("rfkill list bluetooth | grep -q 'blocked: yes'"),
+            err_msg: "failed unblock bluetooth",
+        }
+        .run();
+        if !bl_locked.stdout.is_empty() {
+            ServiceAction {
+                arg: &format!("rfkill unblock bluetooth | sleep 3"),
+                err_msg: "failed unblock bluetooth",
+            }
+            .run();
+        }
+        flag = "on";
+    } else {
+        flag = "off";
+    };
     ServiceAction {
         arg: format!("bluetoothctl power {flag}").as_str(),
         err_msg: "failed to toggle power",
@@ -29,19 +50,121 @@ pub fn toggle_power(power_off: bool) {
     .run();
 }
 
-#[cfg(test)]
+pub fn toggle_scan(scan_on: bool) {
+    if scan_on {
+        ServiceAction {
+            arg: &format!("kill $(pgrep -f 'bluetoothctl --timeout 5 scan on')"),
+            err_msg: "failed to kill scan pids",
+        }
+        .run();
+        ServiceAction {
+            arg: &format!("bluetoothctl scan off"),
+            err_msg: "failed to toggle scan",
+        }
+        .run();
+    } else {
+        let res = ServiceAction {
+            arg: format!("bluetoothctl --timeout 5 scan on &").as_str(),
+            err_msg: "failed to toggle scan",
+        }
+        .run();
+        println!("{:?}", res);
+    }
+}
+
+pub fn toggle_pairable(pairable_on: bool) {
+    let flag = if pairable_on { "off" } else { "on" };
+
+    ServiceAction {
+        arg: &format!("bluetoothctl pairable {flag}"),
+        err_msg: "failed to toggle scan",
+    }
+    .run();
+}
+
+pub fn toggle_discoverable(discoverable_on: bool) {
+    let flag = if discoverable_on { "off" } else { "on" };
+
+    ServiceAction {
+        arg: &format!("bluetoothctl discoverable {flag}"),
+        err_msg: "failed to toggle scan",
+    }
+    .run();
+}
+
+pub fn discoverable_on() -> bool {
+    !check_option_on("Discoverable: yes")
+}
+
+pub fn scan_on() -> bool {
+    !check_option_on("Discovering: yes")
+}
+
+pub fn pairable_on() -> bool {
+    !check_option_on("Pairable: yes")
+}
+
+pub fn power_on() -> bool {
+    check_option_on("Powered: yes")
+}
+
+fn check_option_on(option: &str) -> bool {
+    let command_res = Command::new("sh")
+        .arg("-c")
+        .arg(format!("bluetoothctl show | grep '{option}'"))
+        .output()
+        .expect("failed to execute process");
+    String::from_utf8_lossy(&command_res.stdout).is_empty()
+}
+
+pub fn device_trusted(mac: &str) -> bool {
+    check_device_option_on("Trusted: yes", mac)
+}
+pub fn device_paired(mac: &str) -> bool {
+    check_device_option_on("Paired: yes", mac)
+}
+pub fn device_connected(mac: &str) -> bool {
+    check_device_option_on("Connected: yes", mac)
+}
+
+fn check_device_option_on(option: &str, mac: &str) -> bool {
+    let command_res = Command::new("sh")
+        .arg("-c")
+        .arg(format!("bluetoothctl info {mac} | grep '{option}'"))
+        .output()
+        .expect("failed to execute process");
+    String::from_utf8_lossy(&command_res.stdout).is_empty()
+}
+
+pub fn get_devices() -> Vec<Device> {
+    let res = Command::new("sh")
+        .arg("-c")
+        .arg("bluetoothctl devices")
+        .output()
+        .expect("failed getting devices");
+    let mut devices: Vec<Device> = vec![];
+    for device in String::from_utf8_lossy(&res.stdout).split("\n") {
+        if device.is_empty() {
+            continue;
+        };
+
+        let device_sp = device.split(" ").collect::<Vec<&str>>();
+        let n = device_sp.len();
+
+        devices.push(Device {
+            mac: device_sp[n - 2].to_string(),
+            name: device_sp[n - 1].to_string(),
+        });
+    }
+    devices
+}
+
+#[derive(Debug)]
+pub struct Device {
+    pub mac: String,
+    pub name: String,
+}
+
 mod tests {
     use super::*;
-
-    #[test]
-    fn toggle_on_power() {
-        let output = toggle_power(true);
-        println!("{:?}", output);
-    }
-
-    #[test]
-    fn toggle_of_power() {
-        let output = toggle_power(false);
-        println!("{:?}", output);
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,57 @@
 use rofi;
-use rofi_bluetooth::toggle_power;
-use std::process::Command;
+use rofi_bluetooth::{
+    discoverable_on, get_devices, pairable_on, power_on, scan_on, toggle_discoverable,
+    toggle_pairable, toggle_power, toggle_scan,
+};
 
 fn main() {
     loop {
-        let command_res = Command::new("sh")
-            .arg("-c")
-            .arg("bluetoothctl show | grep 'Powered: yes'")
-            .output()
-            .expect("failed to execute process");
-        let power_off = String::from_utf8_lossy(&command_res.stdout).is_empty();
+        let power_off = power_on();
 
-        let options: Vec<_>;
+        let mut options: Vec<String>;
+        let mut scan: bool = false;
+        let mut discoverable: bool = false;
+        let mut pairable: bool = false;
 
         if power_off {
-            options = vec!["Power: no"];
+            options = vec!["Power: no".to_string()];
         } else {
-            options = vec!["Power: yes"];
+            options = vec![];
+            let devices = get_devices();
+            for device in &devices {
+                options.push(device.name.clone());
+            }
+
+            options.push("----------".to_string());
+            options.push("Power: yes".to_string());
+
+            scan = scan_on();
+            if scan {
+                options.push("Scan: on".to_string());
+            } else {
+                options.push("Scan: off".to_string());
+            }
+
+            discoverable = discoverable_on();
+            if discoverable {
+                options.push("Discoverable: on".to_string());
+            } else {
+                options.push("Discoverable: off".to_string());
+            }
+
+            pairable = pairable_on();
+            if pairable {
+                options.push("Pairable: on".to_string());
+            } else {
+                options.push("Pairable: off".to_string());
+            }
         }
         match rofi::Rofi::new(&options).prompt("Bluetooth").run() {
             Ok(choice) => match choice.as_str() {
                 "Power: no" | "Power: yes" => toggle_power(power_off),
+                "Scan: on" | "Scan: off" => toggle_scan(scan),
+                "Pairable: on" | "Pairable: off" => toggle_pairable(pairable),
+                "Discoverable: on" | "Discoverable: off" => toggle_discoverable(discoverable),
                 _ => println!("{:?}", choice),
             },
             Err(rofi::Error::Interrupted) => {
